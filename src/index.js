932d9611964f2d1d9f48a6ef1f48900cf2b6c20a
fredrik nellbeck <Foggel@live.se>
2024-02-08 09:20:56 +0100

    Some text on the first page.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import index from "./webbpages/index";
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
-    <App />
+    <>
+        <App />
+        <h1>
+            Denna React App är ett pågående project.
+            Kom tillbaka med jämna mellanrum för att se hur den utvecklas.
+        </h1>
+    </>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
