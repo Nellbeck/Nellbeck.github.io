bf1839b8d0c97620218d52c8862858e9efd8ad26
fredrik nellbeck <Foggel@live.se>
2024-02-08 08:34:50 +0100

    Fixed some route problems.

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
             <Sidebar />
             <Routes>
                 <Route
-                    path="/FirstReactApp"
+                    path="/"
                     element={<Index />}
                     
                 />
